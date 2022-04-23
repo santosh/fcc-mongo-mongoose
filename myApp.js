f7ed7b56ab93fd82734487bb89f55e10eb6d8c1c
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-23 05:30:59 +0000

    Soln for findPersonById

--- myApp.js
+++ myApp.js
@@ -4,52 +4,58 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let personSchema = new mongoose.Schema({
-  name: { type: String, require: true },
+  name: String,
   age: Number,
   favoriteFoods: [String]
-})
+});
 
+/** 3) Create and Save a Person */
 let Person = mongoose.model('Person', personSchema);
 
-const createAndSavePerson = (done) => {
-  //... do something (risky) ...
-  const person = new Person({
-    name: 'Santosh Kumar',
-    age: 25,
-    favoriteFoods: ['Golgappa', 'Pizza']
+let createAndSavePerson = function (done) {
+  let janeFonda = new Person({ name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"] });
+
+  janeFonda.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data)
   });
-
-  person.save((error, data) => {
-    if (error) return done(error);
-    done(null, data);
-  })
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  Person.create(arrayOfPeople, (error, data) => {
-    if (error) return done(error);
-    done(null, data);
-  })
+/** 4) Create many People with `Model.create()` */
+let arrayOfPeople = [
+  { name: "Frankie", age: 74, favoriteFoods: ["Del Taco"] },
+  { name: "Sol", age: 76, favoriteFoods: ["roast chicken"] },
+  { name: "Robert", age: 78, favoriteFoods: ["wine"] }
+];
+let createManyPeople = function (arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
-const findPeopleByName = (personName, done) => {
-  Person.find({ name: personName }, (error, persons) => {
-    if (error) return done(error);
-    done(null, persons);
-  })
+/** 5) Use `Model.find()` */
+let findPeopleByName = function (personName, done) {
+  Person.find({ name: personName }, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
-const findOneByFood = (food, done) => {
+/** 6) Use `Model.findOne()` */
+let findOneByFood = function (food, done) {
   Person.findOne({ favoriteFoods: food }, function (err, data) {
-    if (err) {
-      return done(err);
-    }
-    return done(null, data);
+    if (err) return console.log(err);
+    done(null, data);
   });
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+/** 7) Use `Model.findById()` */
+let findPersonById = function (personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
